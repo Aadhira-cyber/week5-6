0d8dc812930a6503e7877b292a5c32da24f424c4
Aadhira <aadhirakshu@gmail.com>
2026-03-17 13:26:38 +0530

    Level2 Question 2 solution

---
+++ Level2/q2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter details for " + names[i] + ":");
+            System.out.print("  Age: ");
+            ages[i] = sc.nextInt();
+            System.out.print("  Height (in cm): ");
+            heights[i] = sc.nextDouble();
+        }
+
+        int youngestIndex = 0;
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+
+        int tallestIndex = 0;
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        System.out.println("\n--- Results ---");
+        System.out.println("Youngest Friend: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
+        System.out.println("Tallest Friend: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
+        
+        sc.close();
+    }
+}
+
